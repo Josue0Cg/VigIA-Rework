31fa161b631e0c955bc30e0e04a69f7070147236
JHONNIGARCIA <22040053@alumno.utc.edu.mx>
2026-04-17 17:37:33 -0600

    fix(FAQ): add missing csrf_exempt import in views.py

--- cross_asistent/views.py
+++ cross_asistent/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.db import models, transaction
 from django.http import JsonResponse
